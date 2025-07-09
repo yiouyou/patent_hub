08a0ad388b90e965b7e69c7ce77cf2d8db5d6ff1
yiouyou <zhuosong@gmail.com>
2025-07-09 16:55:52 +0000

    0710

--- patent_hub/patent_hub/doctype/llm_chat_session/llm_chat_session.py
+++ patent_hub/patent_hub/doctype/llm_chat_session/llm_chat_session.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2025, sz and contributors
 # For license information, please see license.txt
 
+import re
+
 import frappe
 from frappe import _
 from frappe.model.document import Document
@@ -11,5 +13,10 @@ class LLMChatSession(Document):
 	def before_insert(self):
 		if not self.llm_provider:
 			frappe.throw(_("LLM Provider is required to generate Chat ID"))
-		self.chat_id = make_autoname(f"CHAT-{self.llm_provider}-.YY.-.MM.-.DD.-.###")
+		# 获取当前用户 Full Name
+		user_id = frappe.session.user
+		full_name = frappe.db.get_value("User", user_id, "full_name") or user_id
+		clean_name = re.sub(r"[^\w\s-]", "", full_name)
+		safe_name = re.sub(r"\s", "_", clean_name).replace("-", "_")
+		self.chat_id = make_autoname(f"CHAT-{self.llm_provider}-{safe_name}-.YY.-.MM.-.DD.-.###")
 		self.name = self.chat_id
