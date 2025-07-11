5038f1e6ae716ce4d8234b7e3362ee7cd5cf581f
yiouyou <zhuosong@gmail.com>
2025-07-11 06:29:46 +0000

    0711

--- patent_hub/patent_hub/doctype/md_to_docx/md_to_docx.py
+++ patent_hub/patent_hub/doctype/md_to_docx/md_to_docx.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2025, sz and contributors
 # For license information, please see license.txt
 
+import re
+
 import frappe
 from frappe import _
 from frappe.model.document import Document
@@ -9,8 +11,9 @@ from frappe.model.naming import make_autoname
 
 class MDToDocx(Document):
 	def before_save(self):
-		user = frappe.session.user
+		full_name = frappe.get_user().doc.full_name
+		clean_name = re.sub(r"[^a-zA-Z0-9]", "", full_name)
 		# 只有新建时才赋值（也可以根据 is_new 判断）
 		if not self.name or not self.name.startswith("MD2DOCX-"):
-			self.md_to_docx_id = make_autoname(f"MD2DOCX-{user}-.YY.-.MM.-.DD.-.###")
+			self.md_to_docx_id = make_autoname(f"MD2DOCX-{clean_name}-.YY.-.MM.-.DD.-.###")
 			self.name = self.md_to_docx_id
