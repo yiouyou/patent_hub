d21e78f70593cc7ed53a630c8214a0f19275a0e4
yiouyou <zhuosong@gmail.com>
2025-07-22 12:32:38 +0000

    0722

--- patent_hub/patent_hub/doctype/test/test.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, sz and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class test(Document):
-	pass
