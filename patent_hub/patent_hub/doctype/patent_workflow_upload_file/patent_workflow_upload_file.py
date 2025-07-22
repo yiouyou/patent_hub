f8c77a92fa706291441a0689b29d964b74d28620
yiouyou <zhuosong@gmail.com>
2025-07-22 19:31:37 +0000

    0723

---
+++ patent_hub/patent_hub/doctype/patent_workflow_upload_file/patent_workflow_upload_file.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, sz and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class PatentWorkflowUploadFile(Document):
+	pass
