56182283c0fc0fc28bdebe08a6579140174ac306
yiouyou <zhuosong@gmail.com>
2025-08-06 03:37:42 +0000

    0806

--- patent_hub/patent_hub/doctype/patent_agency/patent_agency.py
+++ patent_hub/patent_hub/doctype/patent_agency/patent_agency.py
@@ -11,10 +11,10 @@ from frappe.model.naming import make_autoname
 
 class PatentAgency(Document):
 	def before_insert(self):
-		if not self.agency_name:
-			frappe.throw(_("Agency Name is required to generate Patent Agency"))
-		if not self.weixin:
-			frappe.throw(_("Weixin is required to generate Patent Agency"))
-		safe_name = re.sub(r"[^\w\s-]", "", self.agency_name).replace(" ", "_")
+		if not self.agent_name:
+			frappe.throw(_("Agent Name is required to generate Patent Agency"))
+		# if not self.weixin:
+		# 	frappe.throw(_("Weixin is required to generate Patent Agency"))
+		safe_name = re.sub(r"[^\w\s-]", "", self.agent_name).replace(" ", "_")
 		self.agency_id = make_autoname(f"AGY-{safe_name}-.##")
 		self.name = self.agency_id
